f43a4b763fd9fcc68fdfa907c26d1ac2b30bb584
yuhanglili <157473333+yuhanglili@users.noreply.github.com>
2024-08-27 21:06:00 +0800

    Update genetate_source_dataset.py

--- genetate_source_dataset.py
+++ genetate_source_dataset.py
@@ -3,22 +3,22 @@ import numpy as np
 
 
 
-f=h5py.File('./data/source_data/BO-13-13-100.h5','r')
+f=h5py.File('./data/source_data/BO-28-28-100.h5','r')
 data1=f['data'][:]
 f.close()
 
 
-f=h5py.File('./data/source_data/HS-13-13-100.h5','r')
+f=h5py.File('./data/source_data/HS-28-28-100.h5','r')
 data2=f['data'][:]
 f.close()
 
 
-f=h5py.File('./data/source_data/CH-13-13-100.h5','r')
+f=h5py.File('./data/source_data/CH-28-28-100.h5','r')
 data3=f['data'][:]
 f.close()
 
 
-f=h5py.File('./data/source_data/KSC-13-13-100.h5','r')
+f=h5py.File('./data/source_data/KSC-28-28-100.h5','r')
 data4=f['data'][:]
 f.close()
 
@@ -32,6 +32,6 @@ data=np.vstack((data1,data2,data3,data4))
 print(data.shape) 
 
 
-f=h5py.File('./data/source_data/13_13_meta_train_' + str(data.shape[0]) + '_' + str(data.shape[1]) + '.h5','w')
+f=h5py.File('./data/source_data/meta_train_' + str(data.shape[0]) + '_' + str(data.shape[1]) + '.h5','w')
 f['data']=data
 f.close()
